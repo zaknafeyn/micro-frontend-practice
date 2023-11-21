08959f1e270965a394e982e18cac6a3066aa06b7
Valentyn Radchuk <zaknafeyn@gmail.com>
2023-11-21 08:59:27 +0100

    fix

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,8 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+        // marketing: `marketing@${domain}/marketing/remoteEntry.js`
+        marketing: `marketing@${domain}/remoteEntry.js`
       },
       shared: packageJson.dependencies
     })
