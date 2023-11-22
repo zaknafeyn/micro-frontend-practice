1b75679ddfaad4c663ca2c64d8462f0fdd68e3ae
Valentyn Radchuk <zaknafeyn@gmail.com>
2023-11-22 12:47:21 +0100

    Add prefix for prod styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co-'
+});
+
 export default () => (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
-  </BrowserRouter>
+  <StylesProvider generateClassName={generateClassName}>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+      </BrowserRouter>
+  </StylesProvider>
 );
