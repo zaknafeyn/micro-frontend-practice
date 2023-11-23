bc13631d895b5206deaaf746c9dbe9a92fb6673e
Valentyn Radchuk <zaknafeyn@gmail.com>
2023-11-23 13:03:50 +0100

    Add Lazy loading and Auth emulation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {lazy, Suspense, useState} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import AuthApp from './components/AuthApp';
-import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import Progress from './components/Progress';
+
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co-'
 });
 
-export default () => (
-  <StylesProvider generateClassName={generateClassName}>
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/auth" component={AuthApp} />
-          <Route path="/" component={MarketingApp} />
-        </Switch>
-      </div>
-      </BrowserRouter>
-  </StylesProvider>
-);
+export default () => {
+
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)}/>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+        </BrowserRouter>
+    </StylesProvider>
+  )
+};
