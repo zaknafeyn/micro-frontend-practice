eb8ac532e93389bcd1d9ed633660e154463c3645
Valentyn Radchuk <zaknafeyn@gmail.com>
2023-11-23 09:39:21 +0100

    Handle browser navigation in dev mode for marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App';
 
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -28,7 +28,7 @@ const mount = (el, { onNavigate }) => {
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.getElementById('_marketing-dev-root');
-  if (devRoot) mount(devRoot, {});
+  if (devRoot) mount(devRoot, {defaultHistory: createBrowserHistory()});
 }
 
 export { mount };
